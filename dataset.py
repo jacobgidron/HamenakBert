ece4b6cf8e05e60fdda014dbf8526d89c1aa908a
idan10000 <49813824+idan10000@users.noreply.github.com>
2022-02-10 18:25:43 +0200

    changed Data class to a dataset for hugging face trainer

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 
 from cachier import cachier
+from torch.utils.data import Dataset
 from transformers import AutoTokenizer
 
 import pre_processing
@@ -72,84 +73,38 @@ def merge(texts, tnss, nss, dss, sss):
     return res
 
 
-
-
-class Data:
-    text: np.ndarray = None
-    normalized: np.ndarray = None
-    dagesh: np.ndarray = None
-    sin: np.ndarray = None
-    niqqud: np.ndarray = None
-
-    filenames: Tuple[str, ...] = ()
-
-    @staticmethod
-    def concatenate(others):
+class textDataset(Dataset):
+    def __init__(self, base_paths, maxlen):
         """
-        Used as a constructor
-        :param others:
-        :return:
+        Args:
         """
-        self = Data()
-        self.text = np.concatenate([x.text for x in others])
-        self.normalized = np.concatenate([x.normalized for x in others],dtype=object)
-        self.dagesh = np.concatenate([x.dagesh for x in others])
-        self.sin = np.concatenate([x.sin for x in others])
-        self.niqqud = np.concatenate([x.niqqud for x in others])
-        return self
+        self.data = []
+        corpora = read_corpora(base_paths)
+        for (filename, heb_items) in corpora:
+            text, normalized, dagesh, sin, niqqud = zip(
+                *(zip(*line) for line in pre_processing.split_by_length(heb_items, maxlen)))
+            niqqud = niqqud_table.to_ids(niqqud)
+            dagesh = dagesh_table.to_ids(dagesh)
+            sin = sin_table.to_ids(sin)
 
-    def shapes(self):
-        return self.text.shape, self.normalized.shape, self.dagesh.shape, self.sin.shape, self.niqqud.shape  # , self.kind.shape
-
-    def shuffle(self):
-        utils.shuffle_in_unison(
-            self.text,
-            self.normalized,
-            self.dagesh,
-            self.niqqud,
-            self.sin
-        )
-        return self
-
-    @staticmethod
-    def from_text(heb_items, maxlen: int) -> 'Data':
-        assert heb_items
-        self = Data()
-        text, normalized, dagesh, sin, niqqud = zip(*(zip(*line) for line in pre_processing.split_by_length(heb_items, maxlen)))
-
-        def pad(ords, dtype='int32', value=0):
-            return utils.pad_sequences(ords, maxlen=maxlen, dtype=dtype, value=value)
-
-        self.normalized = tuple("".join(x) for x in normalized)
-        self.dagesh = dagesh_table.to_ids(dagesh)
-        self.sin = sin_table.to_ids(sin)
-        self.niqqud = niqqud_table.to_ids(niqqud)
-        self.text = text
-        return self
+            for i in range(len(text)):
+                item = {
+                    "text": "".join(normalized[i]),
+                    "y1": {'N': niqqud[i], 'D': dagesh[i], 'S': sin[i]},
+                }
+                self.data.append(item)
 
     def __len__(self):
-        return self.normalized.shape[0]
+        return len(self.data)
 
-    def print_stats(self):
-        print(self.shapes())
+    def __getitem__(self, idx):
+        return self.data[idx]
 
 
 def read_corpora(base_paths):
-    return tuple([(filename, list(pre_processing.iterate_file(filename))) for filename in utils.iterate_files(base_paths)])
+    return tuple(
+        [(filename, list(pre_processing.iterate_file(filename))) for filename in utils.iterate_files(base_paths)])
 
-
-@cachier()
-def load_data(base_paths, maxlen: int) -> Data:
-    corpora = read_corpora(base_paths)
-    corpus = [(filename, Data.from_text(heb_items, maxlen)) for (filename, heb_items) in corpora]
-    cs = [c for (_, c) in corpus]
-    random.shuffle(cs)
-    return Data.concatenate(cs)
-
-def get_xy(d):
-    x = d.normalized
-    y = {'N': d.niqqud, 'D': d.dagesh, 'S': d.sin}
-    return (x, y)
 
 if __name__ == '__main__':
     # data = Data.concatenate([Data.from_text(x, maxlen=64) for x in read_corpora(['test1.txt'])])
@@ -157,9 +112,10 @@ if __name__ == '__main__':
     # print(np.concatenate([data.normalized[:1], data.sin[:1]]))
     # res = merge(data.text[:1], data.normalized[:1], data.niqqud[:1], data.dagesh[:1], data.sin[:1])
     # print(res)
-    train_dict = {}
-    train_dict["test"] = get_xy(load_data(tuple(['test1.txt']), maxlen=16).shuffle())
+    # train_dict = {}
+    # train_dict["test"] = get_xy(load_data(tuple(['test1.txt', 'test2.txt']), maxlen=16).shuffle())
+    testData = textDataset(tuple(['test1.txt', 'test2.txt']), 16)
     print_tables()
     print(letters_table.to_ids(["שלום"]))
 
-load_data.clear_cache()
+# load_data.clear_cache()
