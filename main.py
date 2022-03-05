7dec74ff162e9bc4600144c0557984ec46d0fac0
idan10000 <49813824+idan10000@users.noreply.github.com>
2022-03-05 17:12:11 +0200

    add hydra base config

--- main.py
+++ main.py
@@ -25,85 +25,74 @@ Val_BatchSize = 32
 train_data = [TRAIN_PATH]
 val_data = [VAL_PATH]
 test_data = [TEST_PATH]
-
 DROPOUT = 0.1
 Train_BatchSize = 32
 LR = 1e-5
 MAX_EPOCHS = 100
 MIN_EPOCHS = 5
+def setup_model(train_data, val_data, test_data):
+    # init data module
+    dm = HebrewDataModule(train_data, val_data, test_data)
+    dm.setup()
 
-# init data module
-dm = HebrewDataModule(
-    train_paths=train_data,
-    val_path=val_data,
-    test_paths=test_data,
+    steps_per_epoch = len(dm.train_data) // Train_BatchSize
+    total_training_steps = steps_per_epoch * MAX_EPOCHS
+    warmup_steps = total_training_steps // 5
 
-    model=MODEL,
-    max_seq_length=MAX_LEN,
-    train_batch_size=Train_BatchSize,
-    val_batch_size=Val_BatchSize)
-dm.setup()
+    # init module
+    model = MenakBert(n_warmup_steps=warmup_steps, n_training_steps=total_training_steps)
 
-steps_per_epoch = len(dm.train_data) // Train_BatchSize
-total_training_steps = steps_per_epoch * MAX_EPOCHS
-warmup_steps = total_training_steps // 5
+def train_model(model, dm):
+    # config training
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="checkpoints",
+        filename="best-checkpoint",
+        save_top_k=1,
+        verbose=True,
+        monitor="train_loss",
+        mode="min"
+    )
+    logger = TensorBoardLogger("lightning_logs", name="nikkud_logs")
+    early_stopping_callback = EarlyStopping(monitor='train_loss', patience=200)
 
-# init module
-model = MenakBert(model=MODEL ,
-                  dropout= DROPOUT,
-                  train_batch_size= Train_BatchSize,
-                  max_epochs=MAX_EPOCHS,
-                  min_epochs= MIN_EPOCHS,
-                  n_warmup_steps=warmup_steps,
-                  n_training_steps=total_training_steps)
+    trainer = Trainer(
+        logger=logger,
+        auto_lr_find=True,
+        checkpoint_callback=checkpoint_callback,
+        callbacks=[early_stopping_callback],
+        max_epochs=20,
+        gpus=1,
+        progress_bar_refresh_rate=1,
+        log_every_n_steps=1
+    )
+    trainer.tune(model)
+    trainer.fit(model, dm)
+    return trainer
 
-# config training
-checkpoint_callback = ModelCheckpoint(
-    dirpath="checkpoints",
-    filename="best-checkpoint",
-    save_top_k=1,
-    verbose=True,
-    monitor="train_loss",
-    mode="min"
-)
-logger = TensorBoardLogger("lightning_logs", name="nikkud_logs")
-early_stopping_callback = EarlyStopping(monitor='train_loss', patience=200)
+def eval_model(trainer, dm):
+    # eval
+    tokenizer = dm.tokenizer
+    val_dataset = textDataset(
+        [VAL_PATH],
+        MAX_LEN,
+        tokenizer
+    )
 
-trainer = Trainer(
-    logger=logger,
-    # auto_lr_find=True,
-    checkpoint_callback=checkpoint_callback,
-    callbacks=[early_stopping_callback],
-    max_epochs=2,
-    gpus=1,
-    progress_bar_refresh_rate=1,
-    log_every_n_steps=1
-)
-# trainer.tune(model)
-trainer.fit(model, dm)
+    sample_batch = next(iter(DataLoader(val_dataset, batch_size=8, num_workers=2)))
 
-# eval
-tokenizer = dm.tokenizer
-val_dataset = textDataset(
-    [VAL_PATH],
-    MAX_LEN,
-    tokenizer
-)
+    trained_model = MenakBert.load_from_checkpoint(
+        trainer.checkpoint_callback.best_model_path,
+    )
+    trained_model.eval()
+    trained_model.freeze()
 
-sample_batch = next(iter(DataLoader(val_dataset, batch_size=8)))
+    # what is this
+    input = sample_batch["input_ids"]
+    mask = sample_batch["attention_mask"]
+    _, predictions = trained_model(input, mask)
 
-trained_model = MenakBert.load_from_checkpoint(
-    trainer.checkpoint_callback.best_model_path,
-)
-trained_model.eval()
-trained_model.freeze()
-
-input = sample_batch["input_ids"]
-mask = sample_batch["attention_mask"]
-_, predictions = trained_model(input, mask)
-
-pred = np.argmax(predictions['N'].cpu().detach().numpy(), axis=-1)
-labels = sample_batch["label"]['N'].cpu().detach().numpy()
+    pred = np.argmax(predictions['N'].cpu().detach().numpy(), axis=-1)
+    labels = sample_batch["label"]['N'].cpu().detach().numpy()
 
 plt.figure(figsize=(12, 7))
 classes = ["none", "rafa", "shva", "hataph segol", "hataph patah", "hataph kamats", "hirik", "chere", "segol", "phatah",
